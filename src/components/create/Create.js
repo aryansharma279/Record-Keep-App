3a6eac77b486bc425e812b88553838b508e9f0a7
Aryan Sharma <developer.aryansharma@gmail.com>
2023-02-08 17:22:55 +0530

    [NEW] Added changes in creating and deleting of a record

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -4,42 +4,48 @@ import Stack from "@mui/material/Stack";
 import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
 import "./Create.css";
-import Display from "../display/Display";
+// import Display from "../display/Display";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 const Create = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [form, setForm] = useState({});
   const [data, setData] = useState([]);
   const addData = () => {
-    setData([...data, { name, email }]);
-    setName("");
-    setEmail("");
+    const emp = {
+      name: '',
+      email: ''
+    }
+    setData([...data, form ]);
+    setForm(form);
+    setForm(emp);
+  };
+  const removeItem = (i) => {
+    console.log(data);
+    data.splice(i, 1);
+    console.log(data);
+    setData([...data]);
   };
   return (
     <div className="inputs">
       <Stack direction="row" spacing={2}>
         <TextField
-          value={name}
-          onChange={(event) => setName(event.target.value)}
+          value={form.name}
+          onChange={(event) => setForm({...form, name: event.target.value })}
           className="field"
           id="standard-basic"
           label="Name "
           variant="standard"
         />
         <TextField
-        type='email'
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          type="email"
+          value={form.email}
+          onChange={(event) => setForm({...form,email: event.target.value })}
           className="field"
           id="standard-basic"
           label="Email"
           variant="standard"
         />
-        <Button
-          variant="contained"
-          onClick={addData}
-          color="success"
-        >
+        <Button variant="contained" onClick={addData} color="success">
           <AddIcon />
         </Button>
       </Stack>
@@ -48,15 +54,22 @@ const Create = () => {
           <h3>Name</h3>
           <h3>Email</h3>
           <h3>Action</h3>
-          
         </div>
-        {data.map((element,index)=> {
-             return(
-             <Display {...element} index={index} key={index} />
-             )
+        {data.map((element, index) => {
+          return (
+            <div key={index} className="outputs">
+              <h4>{element.name}</h4>
+              <h4>{element.email}</h4>
+              <Button
+                onClick={() => removeItem(index)}
+                variant="contained"
+                color="error"
+              >
+                <RemoveIcon />
+              </Button>
+            </div>
+          );
         })}
-       
-        
       </div>
     </div>
   );
