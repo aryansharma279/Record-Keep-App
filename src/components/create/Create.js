8c3b0002d9b7582a620b19b56c2baaa331b11224
Aryan Sharma <developer.aryansharma@gmail.com>
2023-02-08 17:43:58 +0530

    [NEW] Old Code without delete Different Componenet

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -4,7 +4,7 @@ import Stack from "@mui/material/Stack";
 import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
 import "./Create.css";
-// import Display from "../display/Display";
+import Display from "../display/Display";
 import RemoveIcon from "@mui/icons-material/Remove";
 
 const Create = () => {
@@ -57,19 +57,10 @@ const Create = () => {
         </div>
         {data.map((element, index) => {
           return (
-            <div key={index} className="outputs">
-              <h4>{element.name}</h4>
-              <h4>{element.email}</h4>
-              <Button
-                onClick={() => removeItem(index)}
-                variant="contained"
-                color="error"
-              >
-                <RemoveIcon />
-              </Button>
-            </div>
-          );
-        })}
+            <Display {...element} key={index} index={index} />
+              )
+        })
+        }
       </div>
     </div>
   );
